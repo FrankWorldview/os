f0bd52948196e59a50a696326bd70b3a46c26dd3
Frank <frankcpp@gmail.com>
2025-03-10 22:53:11 +0800

    Add files via upload

---
+++ linux/code/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	printf("This is a C program.\n");
+
+	printf("Hello, world!\n");
+
+	return 0;
+}
---
+++ linux/code/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "This is a C++ program." << std::endl;
+
+	std::cout << "Hello, world!" << std::endl;
+
+	return 0;
+}
