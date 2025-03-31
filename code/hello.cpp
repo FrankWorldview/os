503a45d7c17b8b94dc0267d5663fd39f0ec8d03e
Frank <frankcpp@gmail.com>
2025-04-01 00:19:58 +0800

    Update hello.cpp

--- code/hello.cpp
+++ code/hello.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
 int main() {
-	std::cout << "This is a C++ program." << std::endl;
+    std::cout << "This is a C++ program." << std::endl;
 
-	std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
 
-	return 0;
+    return 0;
 }
