7a54efbacd01c2ffb334e56a446e2d847463ca5d
Frank <frankcpp@gmail.com>
2025-04-01 00:15:23 +0800

    Create input.c

---
+++ code/input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    // Prompt for input
+    printf("Enter a number: ");
+    scanf("%d", &number);  // Read an integer
+
+    // Output result
+    printf("Your input: %d\n", number);
+
+    return 0;
+}
