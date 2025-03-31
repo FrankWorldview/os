4724db7d2c4a84fb5e8fb25f4afe45bbb573b777
Frank <frankcpp@gmail.com>
2025-04-01 00:14:16 +0800

    Delete linux/code directory

--- linux/code/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	printf("This is a C program.\n");
-
-	printf("Hello, world!\n");
-
-	return 0;
-}
--- linux/code/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-	std::cout << "This is a C++ program." << std::endl;
-
-	std::cout << "Hello, world!" << std::endl;
-
-	return 0;
-}
